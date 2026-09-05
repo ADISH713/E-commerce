e390bfd3c47b1923fec6bebaa70ec3a41b1430bf
ADISH  C P <adishadi7559@gmail.com>
2026-09-05 14:12:12 +0530

    Styled the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {useDispatch} from 'react-redux';
-import {useNavigate} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {useMutation} from '@tanstack/react-query'
 import { setUser } from '../redux/slices/authSlice';
 import {loginUser} from '../services/userServices'
@@ -25,27 +25,44 @@ function Login() {
         mutate();
     };
   return (
-    <div>
-      <h2>LOGIN</h2>
-      <form onSubmit={handleSubmit}>
+    <div className='min-h-screen flex items-center justify-start p-8 md:p-16 bg-cover bg-center'
+    style={{backgroundImage:"url('/images/LoginBG.png')"}}>
+      <div className='w-full max-w-sm bg-black/60 backdrop-blur-md border border-white/10 rounded-xl p-8 shadow-2xl'>
+      <h1 className='text-2xl font-semi-bold text-white mb-1 tracking-wide'>LOGIN</h1>
+      <p className='text-white text-sm mb-6'>Enter your details to continue</p>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label className="block text-xs font-medium text-white mb-1">Email</label>
         <input
         type="email" 
-        placeholder='Email'
+        placeholder='Enter your email'
         value={email}
         onChange={(e)=>setEmail(e.target.value)}
         required
+        className='w-full placeholder-gray-300 bg-white/10 border border-white/20 rounded-md px-3 py-2 text-sm focus:outline-none focus:border-orange-400 text-white'
         />
+        </div>
+        <div>
+        <label className=" block text-xs font-medium text-white mb-1">Password</label>
         <input 
         type="password"
-        placeholder='Password'
+        placeholder='Enter your Password'
         value={password}
         onChange={(e)=>setPassword(e.target.value)}
         required
+        className='w-full placeholder-gray-300 bg-white/10 border border-white/20 rounded-md px-3 py-2 text-sm focus:outline-none focus:border-orange-400 text-white'
         />
+        </div>
 
-        <button disabled={isPending}>{isPending?'Logging in...':'Login'}</button>
+        <button disabled={isPending}
+        className='w-full bg-orange-600 text-white text-sm font-medium py-2.5 rounded-md hover:bg-orange-700 transition disabled:opacity-60'>{isPending?'Logging in...':'Login'}</button>
         </form>
-        {isError && <p style={{color:'red'}}>{error.message}</p>  }
+        {isError && <p className='text-red-400 text-xs mt-3'>{error.message}</p>  }
+        <p className='text-gray-300 text-xs mt-6 text-center'>
+          Don't have an account?
+          <Link to='/register' className='text-orange-600 font-medium'>Sign up</Link>
+        </p>
+        </div>
 
       
     </div>
